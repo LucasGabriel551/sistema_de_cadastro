5d493c372ab0774b476cf9e0fbcd6a4c73337de3
LucasGabriel551 <lgmm551@gmail.com>
2026-09-14 21:52:23 -0300

    Add function to list clients from database

---
+++ listar.py
@@ -0,0 +1,25 @@
+
+def listar_clientes(banco_dados):
+
+    if len(banco_dados) == 0:
+        print("Nenhum cliente cadastrado.")
+        return None
+
+    print(
+          f"{'ID':<5}"
+          f"{'NOME':<20}"
+          f"{'IDADE':<10}"
+          f"{'TEL.:':<15}"
+          f"{'E-MAIL'}"
+    )
+    print("-" * 60)
+
+    for cliente in banco_dados:
+        print(f"{cliente['id']:<5}"
+              f"{cliente['nome']:<20}"
+              f"{cliente['idade']:<10}"
+              f"{cliente['telefone']:<15}"
+              f"{cliente['email']}"
+        )
+    print("-" * 60)
+    print()
